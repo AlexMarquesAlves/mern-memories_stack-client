f11e80abbe16a9c6841c17e6b136f29a15d2e2be
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-03-26 12:23:47 -0300

    feat: paste the necessary to use styles

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Post from "./Post";
+import useStyles from "./styles";
 
 function Posts() {
+  const classes = useStyles();
+
   return (
     <>
       <h1>Posts</h1>
