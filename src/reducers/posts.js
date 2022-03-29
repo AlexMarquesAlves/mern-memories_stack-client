0ceaf27fa31b7bd0cc4676bdff728635fff131fe
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-03-28 21:03:18 -0300

    Feat: Create getPosts action into actions of redux

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,7 +1,7 @@
 export const reducer = (posts = [], action) => {
   switch (action.type) {
     case "FETCH_ALL":
-      return posts;
+      return action.payload;
     case "CREATE":
       return posts;
 
