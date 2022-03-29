9f2a420f871d1772c47125c64e1a5a05b1f411bb
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-03-28 21:28:18 -0300

    Feat: useState of postData

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import useStyles from "./styles";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 function Form() {
   const classes = useStyles();
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
 
-  return <h1>Form</h1>;
+  const handleSubmit = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label={"Creator"}
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+      </form>
+    </Paper>
+  );
 }
 
 export default Form;
