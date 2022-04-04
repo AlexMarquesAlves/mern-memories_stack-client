00cf978efe03c3bc01b8af11c9bf5c1da48ba75b
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-03 22:59:05 -0300

    Feat: new div for details

--- src/components/Posts/Post/index.jsx
+++ src/components/Posts/Post/index.jsx
@@ -23,16 +23,24 @@ function Post({ post }) {
         image={post.selectedFile}
         title={post.title}
       />
+
       <div className={classes.overlay}>
         <Typography variant="h6">{post.creator}</Typography>
         <Typography variant="body2">
           {moment(post.createdAt).fromNow()}
         </Typography>
       </div>
+
       <div className={classes.overlay2}>
         <Button style={{ color: "white" }} size={"small"} onClick={() => {}}>
           <MoreHorizIcon fontSize="default" />
         </Button>
+      </div>
+
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags.map((tag) => `#${tag} `)}
+        </Typography>
       </div>
     </Card>
   );
