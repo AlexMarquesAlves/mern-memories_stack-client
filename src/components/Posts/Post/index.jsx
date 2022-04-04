e16e807e9fe2364bb8529dc43487a23dde530a80
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-03 22:09:53 -0300

    Feat: new Card from material ui

--- src/components/Posts/Post/index.jsx
+++ src/components/Posts/Post/index.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import useStyles from "./styles";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import moment from "moment";
 
-function Post() {
+function Post({ post }) {
   const classes = useStyles();
 
-  return <h1>Post</h1>;
+  return <Card className={classes.card}></Card>;
 }
 
 export default Post;
