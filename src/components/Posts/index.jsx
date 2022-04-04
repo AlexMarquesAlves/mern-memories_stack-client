5107d303e58f4d27691f645d6bbd2e17d37bc522
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-03 21:58:51 -0300

    Feat: implement logic, by mapping posts

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Post from "./Post";
 import useStyles from "./styles";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 function Posts() {
@@ -9,12 +10,21 @@ function Posts() {
   });
   const classes = useStyles();
 
-  return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.mainContainer}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
