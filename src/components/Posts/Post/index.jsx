76f9c9709894037a7a98428571f78b5342fd3e09
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-04-03 23:00:27 -0300

    Feat: new CardActions to Buttons

--- src/components/Posts/Post/index.jsx
+++ src/components/Posts/Post/index.jsx
@@ -48,6 +48,8 @@ function Post({ post }) {
           {post.message}
         </Typography>
       </CardContent>
+
+      <CardActions className={classes.cardActions}></CardActions>
     </Card>
   );
 }
